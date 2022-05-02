4e0bddb3c75f4cb1a25253b86bf7382f8fed24f3
root <yluo3421@gmail.com>
2022-05-02 11:18:22 -0400

    commit tree eb2a213c9bc7fd6dc848ab2330d135ab9a59fd31

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    var rightNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(rightNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (f) {
+    if (f.key === "ArrowLeft") {
+      moveDodgerRight();
+    }
+  });
